b2034e7ff5d7faaeac426677d7d17cf1a3498946
Ruitao <50489190+Alisrei@users.noreply.github.com>
2025-04-13 04:51:02 +0800

    Add files via upload

---
+++ Enquiry.java
@@ -0,0 +1,62 @@
+package SC2002_Assignment;
+import java.util.Scanner;
+public class Enquiry {
+    private String mainEnq;
+    private String reply;
+    private Boolean replied;
+    private BTOProject project;
+
+    //getters
+    public String getMainEnq() {return mainEnq;}
+    public String getReply() {return reply;}
+    public Boolean getReplied() {return replied;}
+    public BTOProject getProject(){return project;}
+
+    public Enquiry(String m,BTOProject p){
+        this.mainEnq = m;
+        this.reply = "no reply yet";
+        this.replied = false;
+        this.project = p;
+    }
+    public void editEnq(String e){
+        if(this.replied == true){
+            System.out.println("Unable to edit, enquiry has been replied");
+        }
+        else{
+            this.mainEnq = e;
+            System.out.println("Enquiry edited.");
+        }
+    }
+    public void changeProject(BTOProject N){
+        if(this.replied == true){
+            System.out.println("Unable to change, enquiry has been replied");
+        }
+        else{
+            this.project = N;
+            System.out.println("Project changed.");
+        }
+    }
+    public void replyToEnq(String r){
+        this.reply = r;
+        this.replied = true;
+        System.out.println("Enquiry replied.");
+    }
+    public void viewEnq(){
+        System.out.println("Enquiry:" + this.getMainEnq());
+        System.out.println("Reply:" + this.getReply());
+        System.out.println("Project" + this.getProject());
+    }
+
+    public void deleteEnq(){
+        if(this.replied == true){
+            System.out.println("Unable to delete, enquiry has been replied");
+        }
+        else{
+            this.mainEnq = null;
+            this.reply = null;
+            this.replied = null;
+            this.project = null;
+            System.out.println("Enquiry deleted successfully");
+        }
+    }
+}
