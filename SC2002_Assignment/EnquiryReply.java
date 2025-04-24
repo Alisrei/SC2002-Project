c519e288909841e69c176782a97bfb63dcfdd32f
KuGua <67008109+KuGua@users.noreply.github.com>
2025-04-24 20:20:47 +0800

    Update EnquiryReply.java

--- SC2002_Assignment/EnquiryReply.java
+++ SC2002_Assignment/EnquiryReply.java
@@ -1,5 +1,16 @@
 package SC2002_Assignment;
 
+/**
+ * This interface defines the contract for replying to an enquiry.
+ * Classes that implement this interface must provide the functionality to reply to a specific enquiry.
+ */
 public interface EnquiryReply {
+
+    /**
+     * Replies to an enquiry at the specified index with a response for the given BTO project.
+     *
+     * @param index The index of the enquiry to reply to.
+     * @param P The BTO project related to the enquiry.
+     */
     public void replyEnquiry(int index, BTOProject P);
 }
