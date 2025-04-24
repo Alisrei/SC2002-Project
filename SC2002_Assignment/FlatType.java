bfd20b6adf0a9e375707ce081896f87012c5c86f
KuGua <67008109+KuGua@users.noreply.github.com>
2025-04-24 20:22:13 +0800

    Update FlatType.java

--- SC2002_Assignment/FlatType.java
+++ SC2002_Assignment/FlatType.java
@@ -1,23 +1,46 @@
 package SC2002_Assignment;
 
+/**
+ * Enum representing different types of flats available in a BTO project.
+ * Each flat type contains information about the number of rooms and a description.
+ */
 public enum FlatType {
+    
+    /** Represents a two-room flat. */
     TWOROOM(2, "Two Room Flat"),
+
+    /** Represents a three-room flat. */
     THREEROOM(3, "Three Room Flat");
 
     private final int numberOfRooms;
     private final String description;
 
+    /**
+     * Constructor to initialize the flat type with the number of rooms and description.
+     * 
+     * @param numberOfRooms The number of rooms in the flat type.
+     * @param description A short description of the flat type.
+     */
     FlatType(int numberOfRooms, String description) {
         this.numberOfRooms = numberOfRooms;
         this.description = description;
     }
 
+    /**
+     * Gets the number of rooms in the flat type.
+     * 
+     * @return The number of rooms in the flat.
+     */
     public int getNumberOfRooms() {
         return numberOfRooms;
     }
 
+    /**
+     * Gets the description of the flat type.
+     * 
+     * @return The description of the flat type.
+     */
     public String getDescription() {
         return description;
     }
 }
-
