5163cb7c191b893d06a76500ab795bc46a3ff0d4
yc <lee_y_c@outlook.com>
2025-04-24 23:11:53 +0800

    removed yc folder

--- yc/Officer.java
+++
@@ -1,5 +0,0 @@
-public class Officer extends Staff {
-    public Officer(String name, String nric, int age, String maritalStatus, String password) {
-        super(name, nric, age, maritalStatus, password);
-    }
-}
